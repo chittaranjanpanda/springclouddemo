1554fe4035719b879e13205fd38986813728a1cf
Chittaranjan Panda <chittaranjanpanda@eurofins.com>
2018-08-02 14:58:35 +0530

    Adding spring boot admin server for monitoring

--- config-server/src/main/java/com/demo/configserver/ConfigServerApplication.java
+++ config-server/src/main/java/com/demo/configserver/ConfigServerApplication.java
@@ -3,11 +3,11 @@ package com.demo.configserver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication
 @EnableConfigServer
+@SpringBootApplication
 public class ConfigServerApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(ConfigServerApplication.class, args);
     }
