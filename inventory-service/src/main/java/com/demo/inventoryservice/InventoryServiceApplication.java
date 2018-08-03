2a3b5e6a17959a9f205c200b3f8c7613137bced7
Chittaranjan Panda <chittaranjanpanda@eurofins.com>
2018-08-03 16:54:49 +0530

    Adding Feign Proxy Service With Hystrix Fallback

--- inventory-service/src/main/java/com/demo/inventoryservice/InventoryServiceApplication.java
+++ inventory-service/src/main/java/com/demo/inventoryservice/InventoryServiceApplication.java
@@ -5,8 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @EnableDiscoveryClient
+@EnableFeignClients
 @SpringBootApplication
 public class InventoryServiceApplication {
 
